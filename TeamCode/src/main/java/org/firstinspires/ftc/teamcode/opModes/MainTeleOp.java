f197e25c8cdf266bb6aa1200a53f35346f3bf67d
SachetK <71898487+SachetK@users.noreply.github.com>
2021-10-15 19:46:48 -0400

    Fixed Main TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/MainTeleOp.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.opModes;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+
+import org.firstinspires.ftc.teamcode.commands.IntakeCommand;
+import org.firstinspires.ftc.teamcode.commands.OuttakeCommand;
+import org.firstinspires.ftc.teamcode.commands.drive.DriveBackwardCommand;
+import org.firstinspires.ftc.teamcode.commands.drive.DriveForwardCommand;
+import org.firstinspires.ftc.teamcode.subsystems.DrivetrainSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+
+
+
+public class MainTeleOp extends CommandOpMode {
+    //motors
+    private Motor intakeMotor;
+    private Motor leftMotorFront;
+    private Motor leftMotorBack;
+    private Motor rightMotorFront;
+    private Motor rightMotorBack;
+
+    //subsystems
+    private IntakeSubsystem intakeSubsystem;
+    private DrivetrainSubsystem drivetrainSubsystem;
+
+    //commands
+    private IntakeCommand intakeCommand;
+    private OuttakeCommand outtakeCommand;
+    private DriveForwardCommand driveForwardCommand;
+    private DriveBackwardCommand driveBackwardCommand;
+
+    //gamepads
+    private GamepadEx driver;
+
+    @Override
+    public void initialize() {
+        // this.intakeMotor = new Motor(hardwareMap, "intake");
+        this.leftMotorFront = new Motor(hardwareMap, "leftMotorFront");
+        this.rightMotorFront = new Motor(hardwareMap, "rightMotorFront");
+        this.leftMotorBack = new Motor(hardwareMap, "leftMotorBack");
+        this.rightMotorBack = new Motor(hardwareMap, "rightMotorBack");
+        // this.intakeSubsystem = new IntakeSubsystem(this.intakeMotor);
+
+        // this.intakeCommand = new IntakeCommand(this.intakeSubsystem);
+        // this.outtakeCommand = new OuttakeCommand(this.intakeSubsystem);
+        driver = new GamepadEx(gamepad1);
+
+        //driver.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenHeld(this.intakeCommand);
+        //driver.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whenHeld(this.outtakeCommand);
+        driver.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whenHeld(this.driveForwardCommand);
+        driver.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenHeld(this.driveBackwardCommand);
+    }
+}
